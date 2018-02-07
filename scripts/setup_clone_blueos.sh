db79ed18f5ea882cc1d5945782286025faa233c0
Holger E. Jones <jones19@llnl.gov>
2018-02-06 17:39:06 -0800

    setup clone script to bootstrap with all of the submodules, checked out to appropo branches

---
+++ scripts/setup_clone_blueos.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+module load gcc/4.9.3
+module load cmake/3.9.2
+module load spectrum-mpi/2017.11.10
+module load cuda/9.0.176
+
+function is_git_dir() {
+  if [ -d .git ]; then
+    echo .git;
+  else
+    git rev-parse --git-dir 2> /dev/null;
+  fi;
+}
+
+function create_branch() {
+
+git submodule foreach -q --recursive \
+    'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; \
+     [ "$branch" = "" ] && \
+     git checkout master || git checkout $branch' 
+}
+
+if "is_git_dir"; then
+  echo "error .. inside git repo, please change directory"
+else
+  echo "creating directory Laghos-submodules-fresh"
+  mkdir -p "Laghos-submodules-fresh"
+  cd "Laghos-submodules-fresh"
+  pwd
+  git clone git@github.com:jonesholger/Laghos.git --branch feature/raja_with_cali_submodule --recursive
+  cd "Laghos"
+  create_branch
+fi;  
+
+exit 0
+
+
